e6845b0af8242f3e59c18cbeec003721b1dd44d3
imhuwq <imhuwq@gmail.com>
2017-12-03 02:11:58 +0800

    refactor(PlayGround): parenting system

--- PlayGround/ParentingSystem.cpp
+++ PlayGround/ParentingSystem.cpp
@@ -4,9 +4,12 @@
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
 
-    QPushButton button1("test");
-    QPushButton button2("other", &button1);
+    QWidget window;
+    window.setFixedSize(100, 50);
 
-    button1.show();
+    QPushButton * button = new QPushButton("Hello World", &window);
+    button->setGeometry(10, 10, 80, 30);
+
+    window.show();
     return app.exec();
 }
