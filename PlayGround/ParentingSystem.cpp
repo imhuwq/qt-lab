db8a81e2be98878b4ed14d2c0e527e033b9d2ff5
imhuwq <imhuwq@gmail.com>
2017-12-03 02:47:05 +0800

    new(GperfTools): profile with gperftools

--- PlayGround/ParentingSystem.cpp
+++ PlayGround/ParentingSystem.cpp
@@ -7,8 +7,8 @@ int main(int argc, char **argv) {
     QWidget window;
     window.setFixedSize(100, 50);
 
-    QPushButton * button = new QPushButton("Hello World", &window);
-    button->setGeometry(10, 10, 80, 30);
+    QPushButton button("Hello World", &window);
+    button.setGeometry(10, 10, 80, 30);
 
     window.show();
     return app.exec();
