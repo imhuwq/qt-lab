b0ef99cb67ee325cabf0cf4562a956e47d0dcfdc
imhuwq <imhuwq@gmail.com>
2017-12-03 01:45:20 +0800

    new(hello-world): first window

--- PlayGround/HelloWorld.cpp
+++ PlayGround/HelloWorld.cpp
@@ -4,7 +4,15 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    QPushButton button("Hello world!");
+    QPushButton button;
+    button.setText("My Text");
+    button.setToolTip("A tooltip");
+
+    QFont font ("Courier");
+    button.setFont(font);
+
+    button.setIcon(QIcon::fromTheme("face-smile"));
+
     button.show();
 
     return app.exec();
