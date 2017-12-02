5ba8293960517fc98952104b727512df4712fc56
imhuwq <imhuwq@gmail.com>
2017-12-03 02:01:34 +0800

    new(PlayGround): parenting system

---
+++ PlayGround/ParentingSystem.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+
+    QPushButton button1("test");
+    QPushButton button2("other", &button1);
+
+    button1.show();
+    return app.exec();
+}
